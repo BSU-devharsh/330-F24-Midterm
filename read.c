4b4725c00c9c5dceba87e81c67dfec91b60c2e0a
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2024-10-09 14:39:08 -0400

    Update read.c

--- read.c
+++ read.c
@@ -1,7 +1,19 @@
+// Write your name here
+
 #include <stdio.h>
 
 int main() {
     FILE *file;
+    char ch, choice;
+    int lines = 0, words = 0, chars = 0;
+
+    printf("Enter 1 to count lines, words, and characters.\n");
+    printf("Enter 2 to simply print the file.\n");
+    scanf(" %c", &choice);
+
+    // Write your code here
+    // ...
     
+    fclose(file);
     return 0;
 }
