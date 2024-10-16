58f542d8e23a6689273d125714dc11a323cd66a0
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2024-10-16 11:06:43 -0400

    Update read.c

--- read.c
+++ read.c
@@ -2,17 +2,18 @@
 
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]) {
     FILE *file;
     char ch, choice;
     int lines = 0, words = 0, chars = 0;
 
-    printf("Enter 1 to count lines, words, and characters.\n");
-    printf("Enter 2 to simply print the file.\n");
-    scanf(" %c", &choice);
-
+    // Enter 1 to count lines, words, and characters
+    // Enter 2 to print the file simply
+    choice = argv[1];
+    
     // Write your code here
     // ...
+
     
     fclose(file);
     return 0;
